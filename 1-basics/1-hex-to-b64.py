d56aa570ad65b43f7c366b7837a174caa93b7f02
Tim Julian Wörner <tim.woerner@usd.de>
2022-07-28 21:45:49 +0200

    initial commit, exercise 1.1

---
+++ 1-basics/1-hex-to-b64.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import base64
+
+def hex_to_b64(data_hex):
+    data_raw = base64.b16decode(data_hex, casefold=True)
+    data_b64 = base64.b64encode(data_raw)
+    return data_b64
+
+if __name__ == '__main__':
+    test_hex = b'49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d'
+    test_b64 = b'SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t'
+
+    encoded = hex_to_b64(test_hex)
+
+    if (encoded == test_b64):
+        print('Success!')
+    else:
+        print('Failure')
