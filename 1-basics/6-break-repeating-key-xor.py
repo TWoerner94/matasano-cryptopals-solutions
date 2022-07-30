d5d20be2d01b341bfe9048b8818187278e465790
Tim Julian Wörner <tim.woerner@usd.de>
2022-07-30 13:52:25 +0200

    add exercise 1.6

---
+++ 1-basics/6-break-repeating-key-xor.py
@@ -0,0 +1,72 @@
+#!/usr/bin/env python3
+
+import base64
+import math
+from matasano import encode_hex, decode_hex, get_hamming, repeating_key_xor, bruteforce_single_xor, rate_text
+
+if __name__ == '__main__':
+    file = open('6.txt', 'r')
+    data_b64 = file.read()
+
+    # decode from base64
+    data = base64.b64decode(data_b64)
+
+    # bruteforce keysizes
+    hamming_list = []
+    for ks in range(2,40):
+        str1 = data[:ks]
+        str2 = data[ks:ks * 2]
+        str3 = data[ks * 2:ks * 3]
+        str4 = data[ks * 3:ks * 4]
+
+        hamming1 = get_hamming(str1, str2) / ks
+        hamming2 = get_hamming(str2, str3) / ks
+        hamming3 = get_hamming(str3, str4) / ks
+
+        avg_hamming = (hamming1 + hamming2 + hamming3) / 3
+        hamming_list.append({'keysize': ks, 'hamming': avg_hamming})
+
+    hamming_list_sorted = sorted(hamming_list, key=lambda d: d['hamming'])
+    hamming_top = hamming_list_sorted[:3]
+
+    possible_keys = []
+    for h in hamming_top:
+        keysize = h['keysize']
+        
+        # break ciphertext into blocks with length = keysize
+        data_blocks = []
+        for i in range(math.ceil(len(data) / keysize)):
+            lower_bound = keysize * i
+            upper_bound = lower_bound + keysize
+            data_blocks.append(data[lower_bound:upper_bound])
+
+        # build the transposed blocks
+        transposed_blocks = []
+        for i in range(keysize):
+            transposed_b = bytearray(b'')
+            for b in data_blocks:
+                if i < len(b):
+                    transposed_b.append(b[i])
+            transposed_blocks.append(transposed_b)
+
+        # single-character xor each transposed block
+        key = ''
+        for t in transposed_blocks:
+            t_hex = encode_hex(t)
+            key_char = bruteforce_single_xor(t_hex)
+            key += key_char
+        possible_keys.append(key)
+
+    # compare badnesses for the different keys
+    badness_for_keys = []
+    for k in possible_keys:
+        decrypted = decode_hex(repeating_key_xor(data, k))
+        badness = rate_text(decrypted)
+        badness_for_keys.append({'key': k, 'badness': badness, 'plaintext': decrypted})
+
+    # pick the key with the lowest badness
+    min_badness = min(badness_for_keys, key=lambda d: d['badness'])
+
+    # decrypt the text
+    print(min_badness['plaintext'].decode('utf-8'))
+
--- 1-basics/matasano.py
+++ 1-basics/matasano.py
@@ -1,39 +1,43 @@
 import base64
 
-'''decode hexadecimal strings to raw bytes'''
+'''1.1 decode hexadecimal strings to raw bytes'''
 def decode_hex(hex_string):
     return base64.b16decode(hex_string, casefold=True)
 
-'''convert hexadecimal bytes to base64'''
+'''1.1 encode raw bytes to hexadecimal'''
+def encode_hex(raw_bytes):
+    return base64.b16encode(raw_bytes)
+
+'''1.1 convert hexadecimal bytes to base64'''
 def hex_to_b64(data_hex):
     data_raw = decode_hex(data_hex)
     data_b64 = base64.b64encode(data_raw)
     return data_b64
 
-'''xor two strings of the same length'''
+'''1.2 xor two strings of the same length'''
 def fixed_xor(str1, str2):
     str1_raw = decode_hex(str1)
     str2_raw = decode_hex(str2)
     xor_result = (bytes(x ^ y for (x, y) in zip(str1_raw, str2_raw)))
     return xor_result.hex()
 
-'''xor a string with a single character'''
+'''1.3 xor a string with a single byte'''
 def xor_with_key(test_data, key):
     data_raw = decode_hex(test_data)
     return bytes(x ^ key for x in data_raw)
 
-'''rating a piece of text to determine if it is english language plaintext,
+'''1.3 rating a piece of text to determine if it is english language plaintext,
 using the frequency of non-alphabetic characters since the dataset will
 generally be too small for character frequency analysis'''
 def rate_text(text):
-    common_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz '!?.,"
+    common_chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789 '!?.,\n"
     counter = 0
     for i in text:
         if not chr(i) in common_chars:
             counter += 1
     return counter
 
-'''find out what character a string was xor'ed with'''
+'''1.4 find out what character a string was xor'ed with'''
 def bruteforce_single_xor(test_string):
     badness_list = []
     for i in range(0,255):
@@ -46,18 +50,24 @@ def bruteforce_single_xor(test_string):
     # return char that was used for encoding
     return chr(minimum_index)
 
-'''xor a string using a repeating key'''
-def repeating_key_xor(test_string, key):
-    # bring key up to length of test_string
-    test_string_length = len(test_string)
+'''1.5 xor a string using a repeating key'''
+def repeating_key_xor(hex_string, key):
+    test_string_length = len(hex_string)
     key_length = len(key)
 
     result = bytearray(b'')
-    for index, char in enumerate(test_string):
+    for index, char in enumerate(hex_string):
         key_char = index % key_length
         result.append(char ^ ord(key[key_char]))
 
-    return base64.b16encode(result)
+    return encode_hex(result)
 
+'''1.6 compute hamming distance between to strings'''
+def get_hamming(str1, str2):
+    xored = [x ^ y for x, y in zip(str1, str2)]
 
-
+    hamming = 0
+    for x in xored:
+        hamming += bin(x).count('1')
+    
+    return hamming
